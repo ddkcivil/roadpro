5db130c394fd3c587fc11026ea5fa7fe659fbbbd
ddkcivil <dharmadkunwar20@gmail.com>
2026-05-02 12:35:08 +0545

    fix: remove .js extension from imports in api/auth.ts

--- api/auth.ts
+++ api/auth.ts
@@ -1,7 +1,7 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { withErrorHandler } from './utils/errorHandler.js';
-import { getUserByEmail, verifyPassword, generateToken, verifyToken } from './utils/mongoAuth.js';
-import { mapUserFromDb } from './utils/mappers.js';
+import { withErrorHandler } from './utils/errorHandler';
+import { getUserByEmail, verifyPassword, generateToken, verifyToken } from './utils/mongoAuth';
+import { mapUserFromDb } from './utils/mappers';
 
 console.log('[Auth API] Initialized');
 
