846e55b87bb3e0114a4729d54c4ab799f4e1b9a0
ddkcivil <dharmadkunwar20@gmail.com>
2026-06-17 23:12:00 +0545

    2

--- components/modules/MaterialManagementModule.tsx
+++ components/modules/MaterialManagementModule.tsx
@@ -7,6 +7,7 @@ import { Package, FileText, ShoppingCart, BarChart3, Plus } from 'lucide-react';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '~/components/ui/tabs';
 import { Button } from '~/components/ui/button';
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '~/components/ui/dialog';
+import { Input } from '~/components/ui/input';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '~/components/ui/select';
 
 const CATEGORIES = [
